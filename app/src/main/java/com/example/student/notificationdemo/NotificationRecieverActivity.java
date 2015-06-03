7219d6f99696914ebfb84cc1d1a0c9cb626c3658
GitzaWein <gitzaweinstock@gmail.com>
2015-06-03 19:09:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/student/notificationdemo/NotificationRecieverActivity.java
@@ -0,0 +1,16 @@
+package com.example.student.notificationdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by student on 6/1/2015.
+ */
+public class NotificationRecieverActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
